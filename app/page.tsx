1a7e80c2f2dd9a51e97ca90adc911f63474114d9
Andika Rama <116004310+krippooo@users.noreply.github.com>
2025-08-29 13:29:14 +0700

    Update page.tsx
    Resize text doang, ga penting-penting amat

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
     <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mb-4 mt-2 max-w-[1280px] mx-auto px-2'>
       {/* Block T-700 */}
       <div className='flex flex-col items-center bg-card rounded-3xl p-4 text-center border shadow-xs'>
-        <h2 className='text-2xl md:text-3xl font-bold mb-2 text-black'>Effluent (T-700)</h2>
-        <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
-            <DashboardCard 
+        <h2 className='text-xl sm:text-2xl md:text-3xl font-bold mb-2 text-black'>Effluent (T-700)</h2>
+        <div className='grid grid-cols-2 md:grid-cols-4 gap-4 text-sm sm:text-base md:text-lg'>
+            <DashboardCard
               title='COD' 
               count={t700Now?.cod} 
               unit='mg/L'
